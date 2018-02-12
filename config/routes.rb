37abd7b028a70a31984218337e31ebfe48eb20be
mmlumba <mmlumba@gmail.com>
2018-02-12 14:59:55 -0500

    complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
